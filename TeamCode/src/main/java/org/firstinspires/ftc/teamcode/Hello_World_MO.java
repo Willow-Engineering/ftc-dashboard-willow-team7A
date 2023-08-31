fd17237f766f9e304279847c97f17dc55295d111
Matt Owen <matthew_owen@lusherschool.org>
2023-08-31 13:51:50 -0500

    Create Hello_World_MO.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hello_World_MO.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp(name = "Hello_World_MO")
+public class Hello_World_MO extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        //SKJDvnjaefbnkjs
+
+
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+
+        // Wait for the game to start (driver presses PLAY)
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.addData("", "Hello World!!!");
+            telemetry.update();
+        }
+
+    }
+}
