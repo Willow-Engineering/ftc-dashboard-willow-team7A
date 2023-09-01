3cfc654780e9f81033723a3cef673c43a27492fe
Chloe Polin <chloe_polin.stu@willowschoolnola.org>
2023-09-01 10:52:23 -0500

    Update Hello_World_Chloe.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hello_World_Chloe.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hello_World_Chloe.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 public class Hello_World_Chloe extends OpMode {
     @Override
     public void init() {
-        telemetry.addData("Hello", "World");
+        telemetry.addData("Hello","World");
     }
 
     @Override
