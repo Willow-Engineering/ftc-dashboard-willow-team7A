f153ba9a2e03cd72e354c2b86f33bfb909fb3cd2
JacksonKeith1 <jackson_keith.stu@willowschoolnola.org>
2023-09-07 10:13:02 -0500

    space

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variables_Elissa.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variables_Elissa.java
@@ -14,7 +14,7 @@ public class Variables_Elissa extends OpMode {
     public void init() {
         FtcDashboard dashboard = FtcDashboard.getInstance();
         telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
-        
+
         int teamNumber = 19824;
         double motorSpeed = 0.5;
         boolean touchSensorPressed = true;
