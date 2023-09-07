8041e20c5081bb99d978111cc3a7a357abad50d9
Matt Owen <matthew_owen@lusherschool.org>
2023-09-07 10:11:49 -0500

    changed gradle build to 7.4.2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variables_MO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variables_MO.java
@@ -2,30 +2,28 @@ package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 
-@TeleOp(name = "Hello_World_MO")
-public class Variables_MO extends LinearOpMode {
+@TeleOp(name = "Variables_MO")
+public class Variables_MO extends OpMode {
 
     @Override
-    public void runOpMode() {
-
-
+    public void init() {
         FtcDashboard dashboard = FtcDashboard.getInstance();
         telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
 
         // Wait for the game to start (driver presses PLAY)
         telemetry.addData("Status", "Initialized");
         telemetry.update();
+    }
 
-        waitForStart();
-
-        while (opModeIsActive()) {
+    @Override
+    public void loop() {
             telemetry.addData("", "Hello World!!!");
             telemetry.update();
-        }
-
     }
+
 }
+
