598bfeb931557153c9205f879081f68fd381de98
Matt Owen <matthew_owen@lusherschool.org>
2023-09-07 15:02:00 -0500

    Update Variables_MO.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variables_MO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variables_MO.java
@@ -9,10 +9,17 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name = "Variables_MO")
 public class Variables_MO extends OpMode {
 
+
+    int myVar = 0;
+
+
+
     @Override
     public void init() {
         FtcDashboard dashboard = FtcDashboard.getInstance();
         telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+
+
 
         // Wait for the game to start (driver presses PLAY)
         telemetry.addData("Status", "Initialized");
@@ -21,7 +28,15 @@ public class Variables_MO extends OpMode {
 
     @Override
     public void loop() {
-            telemetry.addData("", "Hello World!!!");
+            if(gamepad1.a) {
+                myVar += 5;
+            }
+
+            float yPos = gamepad1.left_stick_y;
+
+            telemetry.addData("myVar is now at: ", myVar);
+            telemetry.addData("A Button: ", gamepad1.a);
+            telemetry.addData("Left Stick Y: ", yPos);
             telemetry.update();
     }
 
