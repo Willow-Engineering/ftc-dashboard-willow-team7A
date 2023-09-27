0776c47f5d553d66afbd77a146853d99cdd50576
Matt Owen <matthew_owen@lusherschool.org>
2023-09-27 10:07:19 -0500

    updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variables_MO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variables_MO.java
@@ -4,41 +4,26 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 
 @TeleOp(name = "Variables_MO")
 public class Variables_MO extends OpMode {
 
-
-    int myVar = 0;
-
-
+    private DcMotor motor1;
 
     @Override
     public void init() {
         FtcDashboard dashboard = FtcDashboard.getInstance();
         telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
-
-
-
-        // Wait for the game to start (driver presses PLAY)
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
     }
-
     @Override
     public void loop() {
+
             if(gamepad1.a) {
-                myVar += 5;
+                motor1.setPower(1);
             }
-
-            float yPos = gamepad1.left_stick_y;
-
-            telemetry.addData("myVar is now at: ", myVar);
-            telemetry.addData("A Button: ", gamepad1.a);
-            telemetry.addData("Left Stick Y: ", yPos);
             telemetry.update();
     }
-
 }
 
